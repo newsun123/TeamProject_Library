0d67fb56db2b84bd2af62febb3e7da92f31dec7f
newsun123 <cyd890216@gmail.com>
2023-08-30 19:50:37 +0900

    update reserveOk

--- abook/src/main/java/com/example/demo/service/ReserveSeatServiceImpl.java
+++ abook/src/main/java/com/example/demo/service/ReserveSeatServiceImpl.java
@@ -64,7 +64,49 @@ public class ReserveSeatServiceImpl implements ReserveSeatService {
 			}
 		}
 
-		return "redirect:/seat/reserveseat";
+		return "redirect:/seat/reserveok?userid="+userid;
+	}
+
+	@Override
+	public String reserveok(HttpServletRequest req, Model model) {
+		String userid=req.getParameter("userid");
+		ReserveSeatVo rvo = mapper.reserveok(userid);
+		model.addAttribute("rvo",rvo);
+		//시간 값이 int로 들어가서 값을 스트링으로 들여보내려고 노가다함.
+		int time9 = rvo.getTime9();
+		int time10 = rvo.getTime10();
+		int time11 = rvo.getTime11();
+		int time12 = rvo.getTime12();
+		int time13 = rvo.getTime13();
+		int time14 = rvo.getTime14();
+		int time15 = rvo.getTime15();
+		int time16 = rvo.getTime16();
+		int time17 = rvo.getTime17();
+		int time18 = rvo.getTime18();
+		
+		String times = "";
+		if(time9 == 1)
+			times = times + "9시~10시, ";
+		if(time10 == 1)
+			times = times + "11시~12시, ";
+		if(time11 == 1)
+			times = times + "13시~14시, ";
+		if(time14 == 1) 
+			times = times + "14시~15시, ";
+		if(time15 == 1) 
+			times = times + "15시~16시, ";
+		if(time16 == 1) 
+			times = times + "16시~17시, ";			
+		if(time17 == 1) 
+			times = times + "17시~18시, ";			
+		if(time18 == 1) 
+			times = times + "18시~19시, ";	
+		String time = times.substring(0, times.length()-2); // 마지막 ,를 자르기 위해 사용
+		
+		
+		model.addAttribute("time",time);
+		
+		return "/seat/reserveok";
 	}
 
 }
