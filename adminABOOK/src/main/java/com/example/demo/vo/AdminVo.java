80cfa721040770ecad7ec1a195d12267ec6d4e12
CHOI YUN DO <141296084+newsun123@users.noreply.github.com>
2023-09-04 09:00:49 +0900

    Delete adminABOOK directory

--- adminABOOK/src/main/java/com/example/demo/vo/AdminVo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.vo;
-
-import lombok.Data;
-
-@Data
-public class AdminVo {
-	private int no;
-	private String bookname,publisher,writer,writetime,bookimg,bookea,bookcode,ect,bwriteday;
-}
