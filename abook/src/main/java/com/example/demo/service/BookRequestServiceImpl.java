1ea2f8d6f4fe789d317e5fbf965ab46274e13728
newsun123 <cyd890216@gmail.com>
2023-09-08 15:43:08 +0900

    ^%

--- abook/src/main/java/com/example/demo/service/BookRequestServiceImpl.java
+++ abook/src/main/java/com/example/demo/service/BookRequestServiceImpl.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
